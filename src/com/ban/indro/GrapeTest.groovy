f62b47322a264329cb6bce84164899632ca6d1d3
Indrashis <indrashis.banerjee@cerner.com>
2019-01-28 18:35:58 +0530

    Adding src

--- src/com/ban/indro/GrapeTest.groovy
+++ src/com/ban/indro/GrapeTest.groovy
@@ -1,6 +1,5 @@
 package com.ban.indro
-@Grab('net.sourceforge.htmlunit:htmlunit:2.8')
-class GrapeTest {
+class GrapeTest implements Serializable {
 def testmeth(){
 println "Inside src dir::::::::::::::::::::::::::::::"
 }
