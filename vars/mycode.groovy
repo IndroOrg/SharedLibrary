9397ba735431e6dd7a082ad15216eeac1c5c8f17
Indrashis <indrashis.banerjee@cerner.com>
2019-01-28 18:37:43 +0530

    Adding src

--- vars/mycode.groovy
+++ vars/mycode.groovy
@@ -1,6 +1,6 @@
 import com.ban.indro
 def call() {
   println "Indrashis Here::::::::::::::::::::"
-  GrapeTest gg=new GrapeTest()
-  gg.testmeth()
+  //GrapeTest gg=new GrapeTest()
+  //gg.testmeth()
 }
