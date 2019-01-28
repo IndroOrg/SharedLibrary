f4a0b2bd295a42c4aa3ea81002f5eff66128129a
Indrashis <indrashis.banerjee@cerner.com>
2019-01-28 18:31:35 +0530

    Adding src

---
+++ src/com/ban/indro/GrapeTest.groovy
@@ -0,0 +1,7 @@
+package com.ban.indro
+@Grab('net.sourceforge.htmlunit:htmlunit:2.8')
+class GrapeTest {
+def testmeth(){
+println "Inside src dir::::::::::::::::::::::::::::::"
+}
+}
--- vars/mycode.groovy
+++ vars/mycode.groovy
@@ -1,3 +1,6 @@
+import com.ban.indro
 def call() {
   println "Indrashis Here::::::::::::::::::::"
+  GrapeTest gg=new GrapeTest()
+  gg.testmeth()
 }
