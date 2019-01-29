5553039418f597463919901bab662920d049f8c9
Indrashis <indrashis.banerjee@cerner.com>
2019-01-29 12:05:47 +0530

    Adding

---
+++ src/com/ban/indro/GrapeTest.groovy
@@ -0,0 +1,7 @@
+package com.ban.indro
+class GrapeTest  implements Serializable{
+
+	def meth1(){
+		println "Working::::::::::::::::::::"
+	}
+}
---
+++ src/com/ban/indro/JenkinsPipeLineSrc.groovy
@@ -0,0 +1,11 @@
+package com.ban.indro
+
+class JenkinsPipeLineSrc {
+
+	def helloWorldWithoutParameter(){
+		println "The Name is Indrashis::::::"
+	}
+	def helloWorldWithParam(String name){
+		println "The Name is ::::::::::::::::$name"
+	}
+}
